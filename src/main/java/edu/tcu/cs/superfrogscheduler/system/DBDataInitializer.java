40ed30445860c0ffeed80d6ed95835599c4e96c7
klein <kienpt2702@gmail.com>
2023-04-14 11:59:34 -0500

    resetup code base resetup code base!

---
+++ src/main/java/edu/tcu/cs/superfrogscheduler/system/DBDataInitializer.java
@@ -0,0 +1,16 @@
+package edu.tcu.cs.superfrogscheduler.system;
+
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+// make this as a Spring bean
+@Component
+public class DBDataInitializer implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+
+}
